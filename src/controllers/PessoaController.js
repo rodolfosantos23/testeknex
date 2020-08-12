3750d7bd3fa9bc86479785fa38c158e0bb15c7f3
rodolfosantos23 <rodolfosantos@msn.com>
2020-08-12 17:00:58 -0300

    Organizando

--- src/controllers/PessoaController.js
+++ src/controllers/PessoaController.js
@@ -3,7 +3,11 @@ const Pessoa = require('../models/Pessoa')
 module.exports = {
 
     async index(req, res) {
-        const pessoa = await Pessoa.query().select('id', 'nome', 'cpf', 'data_nascimento', 'email')
+        const pessoa = await Pessoa.query().select(
+            'id', 'nome', 'cpf', 'data_nascimento', 'email',
+            'rua', 'numero', 'complemento', 'cep', 'bairro', 'cidade', 'estado',
+            'telefone1', 'telefone2'
+        )
         res.json(pessoa)
     },
 
